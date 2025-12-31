cd89335de632cdc643aec8d2852880243487bdb9
Harshal Bhavsar <hb2708@gmail.com>
2025-12-31 16:10:37 +0800

    refactor: Update Input to pass className at div level

--- app/javascript/components/Input.tsx
+++ app/javascript/components/Input.tsx
@@ -5,10 +5,10 @@ import { classNames } from "$app/utils/classNames";
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => (
-  <div className="bg-filled flex h-12 items-center gap-2 rounded border border-border px-4 focus-within:ring-2 focus-within:ring-accent focus-within:outline-none">
+  <div className={classNames("bg-filled flex h-12 items-center gap-2 rounded border border-border px-4 focus-within:ring-2 focus-within:ring-accent focus-within:outline-none", className)}>
     <input
       ref={ref}
-      className={classNames("flex-1 bg-transparent font-[inherit] text-base outline-none", className)}
+      className="flex-1 bg-transparent font-[inherit] text-base outline-none"
       {...props}
     />
   </div>
