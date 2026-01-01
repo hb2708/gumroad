32e1dc78fb395c060d6249e9c1c5ad7e380a9ad0
Harshal Bhavsar <hb2708@gmail.com>
2026-01-02 02:50:40 +0800

    refactor: Toggle to align top

--- app/javascript/components/Toggle.tsx
+++ app/javascript/components/Toggle.tsx
@@ -26,14 +26,14 @@ export const Toggle = ({
       disabled={disabled}
       aria-label={ariaLabel}
       className={classNames(
-        "appearance-none relative h-5 w-[34px] shrink-0 cursor-pointer rounded-full border border-border bg-background",
+        "appearance-none relative h-5 w-[34px] shrink-0 cursor-pointer rounded-full border border-border bg-background align-top",
         "transition-all duration-[140ms] ease-out",
         "checked:bg-accent",
-        "after:absolute after:top-[2px] after:left-[3px] after:size-3.5 after:rounded-full after:bg-current after:transition-all",
-        "checked:after:bg-black checked:after:translate-x-3.5",
+        "after:absolute after:top-1/2 after:left-[3px] after:size-3.5 after:-translate-y-1/2 after:rounded-full after:bg-current after:transition-all",
+        "checked:after:bg-black checked:after:translate-x-3",
         "disabled:opacity-50 disabled:cursor-not-allowed",
       )}
     />
-    {children ? <span className="ml-3 select-none">{children}</span> : null}
+    {children ? <span className="ml-2 cursor-pointer">{children}</span> : null}
   </label>
 );
