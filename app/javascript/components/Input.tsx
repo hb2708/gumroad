fc33da74fcb25b7d01740a6105f597956a4ea02e
Harshal Bhavsar <hb2708@gmail.com>
2026-01-04 23:07:58 +0800

    refactor: Input.tsx to add leading support

--- app/javascript/components/Input.tsx
+++ app/javascript/components/Input.tsx
@@ -3,24 +3,30 @@ import * as React from "react";
 import { classNames } from "$app/utils/classNames";
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  leading?: React.ReactNode;
   trailing?: React.ReactNode;
 };
 
-export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, trailing, ...props }, ref) => (
-  <div
-    className={classNames(
-      "flex h-12 items-center gap-2 rounded border border-border px-4 focus-within:ring-2 focus-within:ring-accent focus-within:outline-none",
-      { "bg-filled": !props.readOnly },
-      className,
-    )}
-  >
-    <input
-      ref={ref}
-      className="flex-1 bg-transparent font-[inherit] text-base text-foreground outline-none"
-      {...props}
-    />
-    {trailing}
-  </div>
-));
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, leading, trailing, ...props }, ref) => (
+    <div
+      className={classNames(
+        "flex h-12 items-center gap-2 rounded border border-border px-4 focus-within:ring-2 focus-within:ring-accent focus-within:outline-none",
+        { "bg-filled": !props.readOnly },
+        props.disabled && "cursor-not-allowed opacity-35",
+        className,
+      )}
+    >
+      {leading}
+      <input
+        ref={ref}
+        className={classNames(
+          "flex-1 bg-transparent font-[inherit] text-base text-foreground outline-none",
+          props.disabled && "cursor-not-allowed",
+        )}
+        {...props}
+      />
+      {trailing}
+    </div>
+  ));
 
 Input.displayName = "Input";
