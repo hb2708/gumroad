bb31c691636f185113add13fe4e61ed7ea273014
Harshal Bhavsar <hb2708@gmail.com>
2026-01-11 09:54:40 +0800

    refactor: Password/New.tsx to use tailwind css

--- app/javascript/pages/User/Passwords/New.tsx
+++ app/javascript/pages/User/Passwords/New.tsx
@@ -5,6 +5,7 @@ import { AuthAlert } from "$app/components/AuthAlert";
 import { Layout } from "$app/components/Authentication/Layout";
 import { SocialAuth } from "$app/components/Authentication/SocialAuth";
 import { Button } from "$app/components/Button";
+import { Input } from "$app/components/Input";
 import { Separator } from "$app/components/Separator";
 import { useOriginalLocation } from "$app/components/useOriginalLocation";
 
@@ -36,18 +37,20 @@ function ForgotPasswordPage() {
       header={<h1>{application_name ? `Connect ${application_name} to Gumroad` : "Forgot password"}</h1>}
       headerActions={<Link href={Routes.login_path({ next })}>Log in</Link>}
     >
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={(e) => void handleSubmit(e)} className="flex flex-col gap-12">
         <SocialAuth />
         <Separator>
           <span>or</span>
         </Separator>
-        <section>
+        <section className="flex flex-col gap-8 pb-12">
           <AuthAlert />
-          <fieldset>
+          <fieldset className="flex flex-col space-y-2">
             <legend>
-              <label htmlFor={uid}>Email to send reset instructions to</label>
+              <label className="cursor-pointer" htmlFor={uid}>
+                Email to send reset instructions to
+              </label>
             </legend>
-            <input
+            <Input
               id={uid}
               type="email"
               value={form.data.user.email}
